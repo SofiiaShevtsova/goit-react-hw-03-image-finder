552dfbe4db689a34468d1c6feb60bec863f69c16
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-12-19 20:22:03 +0200

    add startr file

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Resume from "./CV/CV";
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        display: 'block',
+        alignItems: `center`,
+        color: '#010101',
+      }}
+    >
+      <Resume/>
+    </div>
+  );
+};
